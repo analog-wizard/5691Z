86c922d4f6c09be70ae4fd1c3eec8f038b6614bf
analog-wizard <62163151+analog-wizard@users.noreply.github.com>
2021-06-03 15:28:48 -0500

    Update init.h
    -Updated to most Recent version (21/5/2021) (DD/MM/YYYY)
    -Deleted Vestigial Code (2020-2021 season)
    -Added Annotations

--- 10pt-LRT/include/init.h
+++ 10pt-LRT/include/init.h
@@ -2,26 +2,24 @@
 #define INIT_H
 
 vex::brain brain_;
-
+vex::competition competition_;
 vex::controller controller_;
 
 vex::motor leftFront = vex::motor(vex::PORT16, vex::gearSetting::ratio18_1, false);
 vex::motor leftBack = vex::motor(vex::PORT17, vex::gearSetting::ratio18_1, false);
 vex::motor rightFront = vex::motor(vex::PORT2, vex::gearSetting::ratio18_1, true);
 vex::motor rightBack = vex::motor(vex::PORT5, vex::gearSetting::ratio18_1, true);
-vex::motor rightIntake = vex::motor(vex::PORT9, vex::gearSetting::ratio18_1, true);
-vex::motor leftIntake = vex::motor(vex::PORT14, vex::gearSetting::ratio18_1, false);
-vex::motor ndroller = vex::motor(vex::PORT15, vex::gearSetting::ratio18_1, false);
-vex::motor xyroller = vex::motor(vex::PORT10, vex::gearSetting::ratio18_1, false);
+//The only agreed upon aspect (currently) is a Double Reverse 4 Bar (DR4B)
+vex::motor dr4b = vex::motor(vex::PORT6, vex::gearSetting::ratio36_1, false);
 
-vex::competition competition_;
+//An 8 long array used to store port numbers for installation checks
+// (To see if the motor is in the port specified)
+int8_t port_array[8] = {16, 17, 2, 5, 9, 14, 15, 10};
 
-vex::bumper front_limit = vex::bumper(brain_.ThreeWirePort.H);
-
+//vex::bumper front_limit = vex::bumper(brain_.ThreeWirePort.H);
 vex::inertial imu = vex::inertial(vex::PORT18);
 
-vex::encoder y_encoder = vex::encoder(brain_.ThreeWirePort.A);
-vex::encoder ynd_encoder = vex::encoder(brain_.ThreeWirePort.C);
-vex::encoder x_encoder = vex::encoder(brain_.ThreeWirePort.E);
+vex::encoder y_left_encoder = vex::encoder(brain_.ThreeWirePort.A);
+vex::encoder y_right_encoder = vex::encoder(brain_.ThreeWirePort.C);
 
 #endif
