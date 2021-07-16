e5eb6d710f24981807d4117c3602ece8d971297a
analog-wizard <thecoolmathdude@gmail.com>
2021-07-15 22:06:33 -0500

    Added short hand for different integer types

--- 10pt-LRT/include/init.h
+++ 10pt-LRT/include/init.h
@@ -1,6 +1,15 @@
 #ifndef INIT_H
 #define INIT_H
 
+#define u8 uint8_t
+#define s8 int8_t
+#define u16 uint16_t
+#define s16 int16_t
+#define u32 uint32_t
+#define s32 int32_t
+#define u64 uint64_t
+#define s64 int64_t
+
 vex::brain brain_;
 vex::competition competition_;
 vex::controller controller_;
