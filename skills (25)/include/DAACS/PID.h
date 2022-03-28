6db84f4ec68d5801fae7c804590291891bc34558
analog-wizard <thecoolmathdude@gmail.com>
2022-03-27 20:02:13 -0500

    New Bare-Bones material
    The recollection of the movement library has led to the reconstruction of the functions and classes themselves, so this commit has the bare minimum (excluding the "rotate" function) to move and operate the robot during autonomous.

---
+++ skills (25)/include/DAACS/PID.h
@@ -0,0 +1,100 @@
+/**
+ * Copyright 2019 Bradley J. Snyder <snyder.bradleyj@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+//Modified for simplicity and readabiltiy by Alexander Spicer: 3/15/2022
+
+#ifndef DAACS_PID_H
+#define DAACS_PID_H
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class PID
+{
+    public:
+        PID( double dt, double max, double min, double Kp, double Kd, double Ki );
+        ~PID();
+        double calculate( double setpoint, double pv );
+
+    private:
+        double _dt;
+        double _max;
+        double _min;
+        double _Kp;
+        double _Kd;
+        double _Ki;
+        double _pre_error;
+        double _integral;
+};
+
+
+/**
+ * Implementation
+ */
+PID::PID( double dt, double max, double min, double Kp, double Kd, double Ki ) :
+    _dt(dt),
+    _max(max),
+    _min(min),
+    _Kp(Kp),
+    _Kd(Kd),
+    _Ki(Ki),
+    _pre_error(0),
+    _integral(0)
+{}
+
+double PID::calculate( double setpoint, double pv )
+{
+    
+    // Calculate error
+    double error = setpoint - pv;
+
+    // Proportional term
+    double Pout = _Kp * error;
+
+    // Integral term
+    _integral += error * _dt;
+    double Iout = _Ki * _integral;
+
+    // Derivative term
+    double derivative = (error - _pre_error) / _dt;
+    double Dout = _Kd * derivative;
+
+    // Calculate total output
+    double output = Pout + Iout + Dout;
+
+    // Restrict to max/min
+    if( output > _max )
+        output = _max;
+    else if( output < _min )
+        output = _min;
+
+    // Save error to previous error
+    _pre_error = error;
+
+    return output;
+}
+
+PID::~PID(){}
+
+#endif
--- skills (25)/include/DAACS/main.h
+++ skills (25)/include/DAACS/main.h
@@ -2,17 +2,43 @@
 #define DAACS_MAIN_H
 
 #include "vision.h"
+#include <vector>
 #include "Vector/main.h"
 #include "Linear/rotation.h"
 #include "Linear/direct.h"
 
 class event_loop {
-  event_loop() {
-    while(true) {};
-  };
-  ~event_loop();
-
+  public:
+    event_loop() {};
+    ~event_loop();
   
+};
+
+class field : public event_loop {
+  public:
+    typedef struct angle_3d {
+      double yaw;
+      double pitch;
+      double roll;
+    } angles;
+    angles angle_3d;
+    
+    typedef struct game_obj {
+      double yaw;
+      double pitch;
+      double roll;
+    } element_obj;
+
+    double x_pos;
+    double y_pos;
+    std::vector<game_obj> obj_vect;
+
+    field(double x_pos, double y_pos, angles angle_, std::vector<game_obj> obj_vect_) : event_loop() {
+      x_pos = x_pos;
+      y_pos = y_pos;
+      field::angle_3d = angle_;
+      obj_vect = obj_vect_;
+    };
 };
 
 #endif
